5c54304e7cef3448e8bb3d0aed537271fc6653af
AutomationInline <samineni007@gmail.com>
2017-11-16 18:25:59 +0530

    Added Cucumber features and classes

---
+++ src/test/java/features/HomePage.feature
@@ -0,0 +1,7 @@
+Feature: LoginFeature
+  This feature deals with the HomePage functionality of the application
+
+  Scenario: Verify read me section of Cucumber project
+
+    Given I navigate to GitHub cucumber project landing page
+    Then I see read me section about the project
---
+++ src/test/java/steps/HomePageStep.java
@@ -0,0 +1,44 @@
+package steps;
+
+import Base.BaseUtil;
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import pages.HomePage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Karthik on 10/15/2016.
+ */
+public class HomePageStep extends BaseUtil{
+
+    private  BaseUtil base;
+
+    public HomePageStep(BaseUtil base) {
+        this.base = base;
+    }
+
+
+    @Given("^I navigate to GitHub cucumber project landing page$")
+    public void i_navigate_to_GitHub_cucumber_project_landing_page() throws Throwable {
+
+        base.Driver.navigate().to("https://github.com/AutomationInine/Cucumber");
+
+    }
+
+    @Then("^I see read me section about the project$")
+    public void i_see_read_me_section_about_the_project() throws Throwable {
+
+        HomePage homePage = new HomePage(base.Driver);
+       String title = homePage.getTitile();
+        org.testng.Assert.assertEquals("GitHub - AutomationInine/Cucumber: This is a simple Cucumber framework designed to write UI test scripts using Selenium and Java",title);
+
+    }
+
+}
